314306da2413730ffe340bc375c41f9fdafd1062
aidenowo <as9135566@gmail.com>
2022-01-22 22:01:58 +0800

    com

---
+++ Assets/LoomObjectSystem/code/wall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "trap")
+        {
+            
+            other.transform.position = new Vector3(Random.Range(-8.6f, 8.29f), Random.Range(-4.3f, 4.6f), 500f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
