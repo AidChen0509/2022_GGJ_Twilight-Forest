d7855e5f1076333d9d8ec2a57191c96b4f782cc6
aidenowo <as9135566@gmail.com>
2022-01-22 14:02:25 +0800

    PathFinding

--- Assets/script/swamptrigger.cs
+++ Assets/script/swamptrigger.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class swamptrigger : MonoBehaviour
 {
     playermove player = null;
-
+    
     // Start is called before the first frame update
     void Start()
     {
