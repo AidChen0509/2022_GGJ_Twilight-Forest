1f45a5d591c17b2b41db790994c5a56b949541fc
aidenowo <as9135566@gmail.com>
2022-01-23 03:51:44 +0800

    title1 begin set
    需要有音樂做背景

--- Assets/storySystem/scripts/Story1Control.cs
+++ Assets/storySystem/scripts/Story1Control.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class StoryBegin : MonoBehaviour
+public class Story1Control : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +14,5 @@ public class StoryBegin : MonoBehaviour
     void Update()
     {
         
-    }
-    public void startmenu()
-    {
-        SceneManager.LoadScene("stage1");
-    }
-    public void endgame()
-    {
-        Application.Quit();
     }
 }
