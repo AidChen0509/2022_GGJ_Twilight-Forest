a96349eaee605ae3268ae211f9b0703e9178e618
shiunglin1017 <martin19991017@gmail.com>
2022-01-23 11:04:00 +0800

    add music environment

--- Assets/storySystem/scripts/musicNotstop.cs
+++ Assets/storySystem/scripts/musicNotstop.cs
@@ -5,15 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class musicNotstop : MonoBehaviour
 {
+    public AudioClip[] audios;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(SceneManager.GetActiveScene().name == "Storyscene1")
+        {
+
+            this.GetComponent<AudioSource>().clip = audios[0];
+            this.GetComponent<AudioSource>().Play();
+        }
+        if (SceneManager.GetActiveScene().name == "Storyscene1")
+        {
+
+            this.GetComponent<AudioSource>().clip = audios[0];
+            this.GetComponent<AudioSource>().Play();
+        }
+        if (SceneManager.GetActiveScene().name == "Storyscene1")
+        {
+
+            this.GetComponent<AudioSource>().clip = audios[0];
+            this.GetComponent<AudioSource>().Play();
+        }
+        if (SceneManager.GetActiveScene().name == "Storyscene1")
+        {
+
+            this.GetComponent<AudioSource>().clip = audios[0];
+            this.GetComponent<AudioSource>().Play();
+        }
+        if (SceneManager.GetActiveScene().name == "Storyscene1")
+        {
+
+            this.GetComponent<AudioSource>().clip = audios[0];
+            this.GetComponent<AudioSource>().Play();
+        }
     }
 }
