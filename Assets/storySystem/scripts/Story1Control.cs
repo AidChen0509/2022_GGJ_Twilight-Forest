5100b1e119fa2b2f57ce4d7c938f714b24a6aca2
shiunglin1017 <martin19991017@gmail.com>
2022-01-23 08:48:35 +0800

    New the story scene 1,2

--- Assets/storySystem/scripts/Story1Control.cs
+++ Assets/storySystem/scripts/Story1Control.cs
@@ -7,21 +7,32 @@ using UnityEngine.SceneManagement;
 public class Story1Control : MonoBehaviour
 {
     public RawImage[] storypicture;
-    
-    
+    public Button nextbutton;
+    public int maxtime = 30;
+    public string nextScenename = "";
+
+    //­p®É
+    float timer_f;
+    int timer_i;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextbutton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer_f = Time.time;
+        timer_i = Mathf.FloorToInt(timer_f);
+        Debug.Log(timer_i);
+        if(timer_i == maxtime)
+        {
+            nextbutton.gameObject.SetActive(true);
+        }
     }
     public void NextLevel()
     {
-        SceneManager.LoadScene("stage1");
+        SceneManager.LoadScene(nextScenename);
     }
 }
